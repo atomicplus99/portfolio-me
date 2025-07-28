7462e671ac35f8b8bc12211490807a7619453b5f
ABJavaDeveloper <abel.ariase.soft@gmail.com>
2025-07-28 18:04:35 -0500

    prueba de environments v2

--- src/environment.ts
+++ src/environment.ts
@@ -3,10 +3,10 @@ declare const process: any;
 export const environment = {
   production: false,
   emailjs: {
-    serviceId: process.env['EMAILJS_SERVICE_ID'],
-    templateId: process.env['EMAILJS_TEMPLATE_ID'],
-    userId: process.env['EMAILJS_USER_ID'],
-    recipient: process.env['EMAILJS_RECIPIENT']
+    serviceId: process.env.EMAILJS_SERVICE_ID,  
+    templateId: process.env.EMAILJS_TEMPLATE_ID,
+    userId: process.env.EMAILJS_USER_ID,
+    recipient: process.env.EMAILJS_RECIPIENT
   }
 };
 
